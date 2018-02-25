b18fc9ec2923cdeb0fb74a93120eecea728d2855
Kasper Mikiewicz <IderedPL@gmail.com>
2018-02-25 14:18:35 +0100

    fix(components): broken icon weight

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -3,22 +3,26 @@ import * as fontawesome from '@fortawesome/fontawesome'
 const FontAwesomeIcon =  require('@fortawesome/react-fontawesome')
 
 export interface Props {
-  name: fontawesome.IconProp,
-  size?: fontawesome.SizeProp,
+  name: fontawesome.IconProp
+  size?: fontawesome.SizeProp
   solid?: boolean
   light?: boolean
   brands?: boolean
 }
 
-export const Icon = ({name, ...props}: Props) => (
+export const Icon = ({name, solid, brands, light, ...props}: Props) => (
   <FontAwesomeIcon
-    icon={Array.isArray(name) ? name : [getType(props), name]}
+    icon={Array.isArray(name) ? name : [getType({solid, brands, light}), name]}
     {...props}
     />
 )
 
-function getType(props: any) {
-  return 'light' in props
-    ? 'fal'
-    : 'solid' in props ? 'fas' : 'brands' in props ? 'fab' : 'far'
-}
+function getType(props: {
+  solid?: boolean
+  light?: boolean
+  brands?: boolean
+}) {
+  return props.light === true ? 'fal'
+    : props.solid === true ? 'fas'
+    : props.brands === true ? 'fab' : 'far'
+  }

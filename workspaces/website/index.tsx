01c059835ef2aa9f04174623209df0979824d0c2
Kasper Mikiewicz <IderedPL@gmail.com>
2018-04-03 08:34:59 +0200

    chore(utils): move service worker to shared utils

--- workspaces/website/index.tsx
+++ workspaces/website/index.tsx
@@ -1,9 +1,10 @@
 import {loadable} from '@shared/utils/loadable'
+import registerServiceWorker from '@shared/utils/register-service-worker'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import registerServiceWorker from './register-service-worker'
 
 const App = loadable(() => import('./app').then(res => res.App || res))
 
 ReactDOM.render(<App />, document.querySelector('#root'))
+
 registerServiceWorker()

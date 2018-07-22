855e3b37e96ac8dc58147f4c6b941f1a76c96e8a
Kasper Mikiewicz <IderedPL@gmail.com>
2018-07-22 14:48:08 +0200

    feat(*): configure jest

--- .config/jest/config.js
+++ .config/jest/config.js
@@ -1,10 +1,13 @@
 module.exports = {
   "moduleNameMapper": {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/mocks/file-mock.js"
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/mocks/file-mock.js",
+    "@shared/(.*)": "<rootDir>/../../workspaces/shared/$1",
+    "@website/(.*)": "<rootDir>/../../workspaces/website/$1"
   },
   "globals": {
     "ts-jest": {
-      "useBabelrc": true
+      "useBabelrc": true,
+      "tsConfigFile": "../../tsconfig.test.json"
     }
   },
   "moduleFileExtensions": [
---
+++ workspaces/website/app.test.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import * as ReactShallowRenderer from 'react-test-renderer/shallow'
+import {App} from './app'
+
+describe('<App />', () => {
+  const renderer = ReactShallowRenderer.createRenderer()
+
+  it('renders', () => {
+    expect(renderer.render(<App />)).toMatchSnapshot()
+  })
+})

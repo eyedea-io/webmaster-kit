1e9099d9ca83525af3f06c40bba6c1762178afb4
Kasper Mikiewicz <IderedPL@gmail.com>
2018-08-06 22:24:52 +0200

    chore(website): hide react hot loader errors

--- workspaces/website/index.tsx
+++ workspaces/website/index.tsx
@@ -2,6 +2,9 @@ import {loadable} from '@shared/utils/loadable'
 import registerServiceWorker from '@shared/utils/register-service-worker'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import {setConfig} from 'react-hot-loader'
+
+setConfig({logLevel: 'no-errors-please'})
 
 const App = loadable(() => import('@website/app').then(res => res.App || res))
 

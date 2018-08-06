92b21d95b9356f77015ed468eb5604b1da6d3aa0
Kasper Mikiewicz <IderedPL@gmail.com>
2018-08-06 22:20:09 +0200

    chore(tests): add reach router mock

---
+++ .config/jest/mocks/reach__router.tsx
@@ -0,0 +1,6 @@
+const React = require('react')
+const rr = require('@reach/router')
+
+rr.Router = ({children}) => <div>{children}</div>
+
+module.exports = rr
--- .config/jest/mocks/react-router-dom.tsx
+++
@@ -1,7 +0,0 @@
-const React = require('react')
-const rrd = require('react-router-dom')
-// Just render plain div with its children
-rrd.BrowserRouter = ({children}) => <div>{children}</div>
-rrd.HashRouter = ({children}) => <div>{children}</div>
-
-module.exports = rrd

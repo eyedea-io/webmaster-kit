ec7b81c5054b707c77708e21ddaf1b063dcc7934
Kasper Mikiewicz <IderedPL@gmail.com>
2018-11-30 15:32:15 +0100

    Add codecov reporter (#33)
    * feat: add codecov reporter
    
    * fix: jest config
    
    * chore: change jest config
    
    * chore: limit number of jest workers

--- .config/jest/config.js
+++ .config/jest/config.js
@@ -1,9 +1,10 @@
 module.exports = {
+  "rootDir": "../../",
   "testURL": "http://localhost:8080/",
   "moduleNameMapper": {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/mocks/file-mock.js",
-    "@shared/(.*)": "<rootDir>/../../workspaces/shared/$1",
-    "@website/(.*)": "<rootDir>/../../workspaces/website/$1"
+    "@shared/(.*)": "<rootDir>/workspaces/shared/$1",
+    "@website/(.*)": "<rootDir>/workspaces/website/$1"
   },
   "globals": {
     "ts-jest": {
@@ -16,16 +17,18 @@ module.exports = {
     "tsx",
     "js"
   ],
-  "roots": [
-    "../../"
-  ],
   "transform": {
     "\\.(ts|tsx)$": "ts-jest"
   },
+  "collectCoverage": true,
+  "coverageDirectory": "./.coverage/",
+  "collectCoverageFrom": [
+    "<rootDir>/workspaces/**/*.{tsx}"
+  ],
   "testMatch": [
-    "**/workspaces/**/*.test.(ts|tsx|js)"
+    "<rootDir>/workspaces/**/*.test.(ts|tsx|js)"
   ],
   "setupFiles": [
-    "<rootDir>/setup-tests.js"
+    "<rootDir>/.config/jest/setup-tests.js"
   ]
 }

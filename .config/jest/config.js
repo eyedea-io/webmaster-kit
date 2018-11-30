2ce581b2875ae0b36327c7858ec84de492e78012
Kasper Mikiewicz <IderedPL@gmail.com>
2018-11-30 15:17:44 +0100

    chore: change jest config

--- .config/jest/config.js
+++ .config/jest/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  "rootDir": "../../",
   "testURL": "http://localhost:8080/",
   "moduleNameMapper": {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/mocks/file-mock.js",
@@ -16,18 +17,16 @@ module.exports = {
     "tsx",
     "js"
   ],
-  "rootDir": "../../",
   "transform": {
     "\\.(ts|tsx)$": "ts-jest"
   },
   "collectCoverage": true,
   "coverageDirectory": "./.coverage/",
   "collectCoverageFrom": [
-    "**/workspaces/**/*.{tsx}",
-    "!**/node_modules/**"
+    "<rootDir>/workspaces/**/*.{tsx}"
   ],
   "testMatch": [
-    "**/workspaces/**/*.test.(ts|tsx|js)"
+    "<rootDir>/workspaces/**/*.test.(ts|tsx|js)"
   ],
   "setupFiles": [
     "<rootDir>/.config/jest/setup-tests.js"

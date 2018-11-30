874fdeaa9b9b59ac13c6f44bae198cad8b5ccdd0
Kasper Mikiewicz <IderedPL@gmail.com>
2018-11-30 15:10:20 +0100

    fix: jest config

--- .config/jest/config.js
+++ .config/jest/config.js
@@ -2,8 +2,8 @@ module.exports = {
   "testURL": "http://localhost:8080/",
   "moduleNameMapper": {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/mocks/file-mock.js",
-    "@shared/(.*)": "<rootDir>/../../workspaces/shared/$1",
-    "@website/(.*)": "<rootDir>/../../workspaces/website/$1"
+    "@shared/(.*)": "<rootDir>/workspaces/shared/$1",
+    "@website/(.*)": "<rootDir>/workspaces/website/$1"
   },
   "globals": {
     "ts-jest": {
@@ -16,22 +16,20 @@ module.exports = {
     "tsx",
     "js"
   ],
-  "roots": [
-    "../../"
-  ],
+  "rootDir": "../../",
   "transform": {
     "\\.(ts|tsx)$": "ts-jest"
   },
   "collectCoverage": true,
-  "coverageDirectory": "../../.coverage/",
+  "coverageDirectory": "./.coverage/",
   "collectCoverageFrom": [
-    "../../workspaces/**/*.{tsx}",
+    "**/workspaces/**/*.{tsx}",
     "!**/node_modules/**"
   ],
   "testMatch": [
     "**/workspaces/**/*.test.(ts|tsx|js)"
   ],
   "setupFiles": [
-    "<rootDir>/setup-tests.js"
+    "<rootDir>/.config/jest/setup-tests.js"
   ]
 }

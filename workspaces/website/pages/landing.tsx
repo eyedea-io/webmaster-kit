89f6f8162b7801372b36bb4fa20a43a3ded9523d
Kasper Mikiewicz <IderedPL@gmail.com>
2019-03-27 10:00:49 +0100

    chore: clean up landing

--- workspaces/website/pages/landing.tsx
+++ workspaces/website/pages/landing.tsx
@@ -1,38 +1,5 @@
 import * as React from 'react'
-import * as UserSelectors from '@website/selectors/user'
-import {routeState} from '@website/state/route'
-import {userState} from '@website/state/user'
-import Kasper from './kasper.jpg'
 
-const ConnectedComponent: React.FC = () => {
-  const [{pathname}] = routeState.use()
-  const [{displayName}, dispatch] = userState.select([UserSelectors.selectDisplayName])
+const Landing: React.FC = () => <div>Hello world</div>
 
-  if (displayName === undefined) {
-    return <img src={Kasper} alt="" />
-  }
-
-  return (
-    <>
-      <div>{pathname}</div>
-      {displayName}
-      <button
-        onClick={() => {
-          dispatch({type: 'setToken', payload: undefined})
-          dispatch({type: 'setProfile', payload: undefined})
-        }}
-      >
-        Logout
-      </button>
-    </>
-  )
-}
-
-const OtherComponent = () => <div>Ook ok</div>
-
-export default () => (
-  <div>
-    <OtherComponent />
-    <ConnectedComponent />
-  </div>
-)
+export default Landing

916d6bb11c5d5f4b68b6a5ba4d96a2724588d493
Kasper Mikiewicz <IderedPL@gmail.com>
2019-03-27 10:01:24 +0100

    chore: remove about page

--- workspaces/website/routes.tsx
+++ workspaces/website/routes.tsx
@@ -5,12 +5,10 @@ import * as React from 'react'
 
 const pages = {
   Index: loadable(() => import('@website/pages/landing')),
-  About: loadable(() => import('@website/pages/about')),
 }
 
 export const Routes = () => (
   <Router basepath={ROUTER_BASEPATH}>
     <pages.Index path="/" />
-    <pages.About path="/about/:id" />
   </Router>
 )

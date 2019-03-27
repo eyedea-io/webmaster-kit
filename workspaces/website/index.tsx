06480298e63b5646f2b8b2a18890fdda1a086b7f
Kasper Mikiewicz <IderedPL@gmail.com>
2019-03-27 09:49:57 +0100

    chore: make App not loadable component

--- workspaces/website/index.tsx
+++ workspaces/website/index.tsx
@@ -1,10 +1,8 @@
 import {registerErrorsListener} from '@shared/utils/errors-listener'
-import {loadable} from '@shared/utils/loadable'
 import * as serviceWorker from '@shared/utils/service-worker'
 import * as React from 'react'
 import {render} from 'react-dom'
-
-const App = loadable(() => import('@website/app'))
+import App from './app'
 
 render(
   <React.Suspense fallback={<div />}>
